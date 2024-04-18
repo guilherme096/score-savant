79679fc0efd595b63e891ab1e310eab4b7cb26f1
guilherme096 <guilherme.rosa60@gmail.com>
2024-04-18 17:58:22 +0100

    initial echo setup

--- app/api/server.go
+++ app/api/server.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type Server struct {
+	listen_add string
+}
+
+func New_server(listen_add string) *Server {
+	return &Server{
+		listen_add: listen_add,
+	}
+}
+
+func (s *Server) Start() {
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hi there :)")
+	})
+
+	e.Logger.Fatal(e.Start(s.listen_add))
+}
--- app/main.go
+++ app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	server "guilherme096/score-savant/api"
+)
 
 func main() {
-	fmt.Println("Hello, Roldao!!")
+	listen_addr := ":8080"
+
+	fmt.Println("App listening on port: ", listen_addr)
+
+	// Create a new server
+	server := server.New_server(listen_addr)
+
+	server.Start()
 }
