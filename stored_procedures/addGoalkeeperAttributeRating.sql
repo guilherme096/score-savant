cbe782191acd3bd2f491b2da67f52c9d97c128c9
JohnnyBoi <jroldao04@ua.pt>
2024-05-23 13:59:36 +0100

    bahh

--- stored_procedures/addGoalkeeperAttributeRating.sql
+++ stored_procedures/addGoalkeeperAttributeRating.sql
@@ -7,7 +7,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE dbo.AddGoalkeeperAttributeRating
+ALTER PROCEDURE dbo.AddGoalkeeperAttributeRating
 (
     @PlayerID INT,
     @Attributes NVARCHAR(MAX)
@@ -18,37 +18,90 @@ BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
 
+        -- Verify and return attributes
         DECLARE @VerifiedAttributes TABLE (Attribute NVARCHAR(255), Rating INT)
         INSERT INTO @VerifiedAttributes
         SELECT Attribute, Rating
         FROM dbo.VerifyAndReturnAttributes(@Attributes)
+
+        -- Debugging: Print contents of @VerifiedAttributes
+        PRINT 'Verified Attributes:'
+        SELECT * FROM @VerifiedAttributes
+
+        -- Ensure attributes exist in at least one of the attribute tables before inserting
+        IF EXISTS (
+            SELECT 1
+            FROM @VerifiedAttributes AS VA
+            WHERE NOT EXISTS (SELECT 1 FROM Goalkeeping_ATT WHERE att_id = VA.Attribute)
+              AND NOT EXISTS (SELECT 1 FROM Mental_ATT WHERE att_id = VA.Attribute)
+              AND NOT EXISTS (SELECT 1 FROM Physical_ATT WHERE att_id = VA.Attribute)
+        )
+        BEGIN
+            PRINT 'One or more attributes do not exist in the attribute tables.'
+            ROLLBACK TRANSACTION;
+            THROW 50002, 'One or more attributes do not exist in the attribute tables.', 1;
+        END
 
         -- Insert into GoalkeeperAttributeRating
         INSERT INTO GoalkeeperAttributeRating (att_id, player_id, rating)
         SELECT Attribute, @PlayerID, Rating
         FROM @VerifiedAttributes
 
-        -- Verify insertion for each category
-        DECLARE @GoalkeepingCount INT, @MentalCount INT, @PhysicalCount INT
+        -- Verify all attributes from Goalkeeping_ATT, Mental_ATT, and Physical_ATT are present
+        DECLARE @MissingGoalkeepingAtt TABLE (att_id NVARCHAR(255))
+        DECLARE @MissingMentalAtt TABLE (att_id NVARCHAR(255))
+        DECLARE @MissingPhysicalAtt TABLE (att_id NVARCHAR(255))
 
-        SELECT @GoalkeepingCount = COUNT(*) FROM Goalkeeping_ATT WHERE att_id IN (SELECT Attribute FROM @VerifiedAttributes)
-        SELECT @MentalCount = COUNT(*) FROM Mental_ATT WHERE att_id IN (SELECT Attribute FROM @VerifiedAttributes)
-        SELECT @PhysicalCount = COUNT(*) FROM Physical_ATT WHERE att_id IN (SELECT Attribute FROM @VerifiedAttributes)
+        -- Find missing goalkeeping attributes
+        INSERT INTO @MissingGoalkeepingAtt
+        SELECT att_id
+        FROM Goalkeeping_ATT
+        WHERE att_id NOT IN (SELECT Attribute FROM @VerifiedAttributes)
 
-        IF @GoalkeepingCount = (SELECT COUNT(*) FROM Goalkeeping_ATT) AND
-           @MentalCount = (SELECT COUNT(*) FROM Mental_ATT) AND
-           @PhysicalCount = (SELECT COUNT(*) FROM Physical_ATT)
+        -- Find missing mental attributes
+        INSERT INTO @MissingMentalAtt
+        SELECT att_id
+        FROM Mental_ATT
+        WHERE att_id NOT IN (SELECT Attribute FROM @VerifiedAttributes)
+
+        -- Find missing physical attributes
+        INSERT INTO @MissingPhysicalAtt
+        SELECT att_id
+        FROM Physical_ATT
+        WHERE att_id NOT IN (SELECT Attribute FROM @VerifiedAttributes)
+
+        -- Print the contents of the missing attribute tables
+        PRINT 'Missing Goalkeeping Attributes:'
+        SELECT * FROM @MissingGoalkeepingAtt
+
+        PRINT 'Missing Mental Attributes:'
+        SELECT * FROM @MissingMentalAtt
+
+        PRINT 'Missing Physical Attributes:'
+        SELECT * FROM @MissingPhysicalAtt
+
+        -- Check if any attributes are missing
+        IF (SELECT COUNT(*) FROM @MissingGoalkeepingAtt) > 0
+           OR (SELECT COUNT(*) FROM @MissingMentalAtt) > 0
+           OR (SELECT COUNT(*) FROM @MissingPhysicalAtt) > 0
         BEGIN
-            COMMIT TRANSACTION;
-        END
-        ELSE
-        BEGIN
+            -- Display missing attributes
+            SELECT 'Missing Goalkeeping Attributes' AS AttributeType, att_id AS MissingAttribute FROM @MissingGoalkeepingAtt
+            UNION ALL
+            SELECT 'Missing Mental Attributes' AS AttributeType, att_id AS MissingAttribute FROM @MissingMentalAtt
+            UNION ALL
+            SELECT 'Missing Physical Attributes' AS AttributeType, att_id AS MissingAttribute FROM @MissingPhysicalAtt
+
             ROLLBACK TRANSACTION;
-            THROW 50001, 'Not all required attributes were inserted.', 1;
+            THROW 50003, 'Not all required attributes were inserted.', 1;
         END
+
+        COMMIT TRANSACTION;
     END TRY
     BEGIN CATCH
-        ROLLBACK TRANSACTION;
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
         THROW;
     END CATCH
 END
+GO
--- stored_procedures/examples/addOutfieldPlayerMock.sql
+++ stored_procedures/examples/addOutfieldPlayerMock.sql
@@ -3,7 +3,7 @@ GO
 
 -- Example call to AddPlayer stored procedure
 EXEC dbo.AddPlayer
-    @name = 'John Doe',
+    @name = 'Jane Doe',
     @age = 25,
     @weight = 75,
     @height = 180,
@@ -18,6 +18,4 @@ EXEC dbo.AddPlayer
     @wage = 50000,
     @contract_end = '2026-06-30',
     @release_clause = 5000000,
-    @attributes = 'Corners:10,Crossing:13,Dribbling:16,Finishing:18,First_Touch:17,Free_Kick_Taking:9,Heading:16,Long_Shots:15,Long_Throws:2,Marking:6,Passing:14,Penalty_Taking:18,Tackling:3,Technique:16,
-					Aggression:10,Anticipation:18,Bravery:17,Composure:18,Concentration:15,Decisons:16,Determination:20,Flair:13,Leadership:15,Off_The_Ball:19,Positioning:3,Teamwork:17,Vision:15,Work_Rate:20,
-					Acceleration:17,Agility:14,Balance:14,Jumping_Reach:16,Natural_Fitness:20,Pace:18,Stamina:20,Strength:17'
+    @attributes = 'Corners:10,Crossing:13,Dribbling:16,Finishing:18,First_Touch:17,Free_Kick_Taking:9,Heading:16,Long_Shots:15,Long_Throws:2,Marking:6,Passing:14,Penalty_Taking:18,Tackling:3,Technique:16,Aggression:10,Anticipation:18,Bravery:17,Composure:18,Concentration:15,Decisons:16,Determination:20,Flair:13,Leadership:15,Off_The_Ball:19,Positioning:3,Teamwork:17,Vision:15,Work_Rate:20,Acceleration:17,Agility:14,Balance:14,Jumping_Reach:16,Natural_Fitness:20,Pace:18,Stamina:20,Strength:17'
